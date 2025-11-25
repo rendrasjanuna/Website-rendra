f7c1b0b4a7f1b2b109a2aba46317916186e83f38
Rendra <rendrasjanuna@gmail.com>
2025-11-25 19:09:52 +0700

    Delete satu.js
    Unused

--- satu.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.getElementById("hamburger");
-const navlinks = document.getElementById("nav-links");
-const logoutBtn = document.getElementById("logoutBtn");
-
-hamburger.addEventListener("click", () => {
-  navlinks.classList.toggle("active");
-});
